48b5ac4f64024e68d77825f7a83eb0ac3e88b010
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2020-08-27 22:54:31 +0700

    Update initial clean architecture

---
+++ src/main/java/com/seastore/usersmicroservices/infrastructure/delivery/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.seastore.usersmicroservices.infrastructure.delivery.controllers;
+
+import com.seastore.usersmicroservices.infrastructure.persistence.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public interface UserController {
+    @PostMapping
+    public User create(@RequestBody User user);
+
+    @GetMapping
+    public List<User> getAll();
+
+    @GetMapping("/{username}")
+    public User getByUsername(@PathVariable("username") String username);
+
+    @PutMapping("/{username}")
+    public ResponseEntity<Integer> updateByUsernameAndPassword(@PathVariable("username") String username,
+                                                               @RequestBody User userToUpdate);
+}
+
