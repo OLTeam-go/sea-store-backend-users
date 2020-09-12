96b601e6f3e093ecdf4044c41598cf8b3008bf33
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2020-09-12 11:21:47 +0700

    Update wallet balance data type

--- src/main/resources/db/migration/V2__WalletsTable.sql
+++ src/main/resources/db/migration/V2__WalletsTable.sql
@@ -2,7 +2,7 @@ create table Wallets
 (
     ID         uuid primary key not null,
     User_ID    uuid             not null,
-    Balance    money            not null,
+    Balance    numeric          not null,
     Created_At timestamp        not null,
     Updated_At timestamp        not null,
     CONSTRAINT FK_Wallet_Users
